2d3d6f8fe99dbed96ca8644ad277d2f027cb96c7
Zeyad Gamal <99384081+Zeyad-Gamal@users.noreply.github.com>
2024-01-07 01:22:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,109 +13,125 @@ window.onscroll = () =>{
 
 };
 
-var swiper = new Swiper(".home-slider", {
-    loop:true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl : ".swiper-button-prev", 
-    },
-});
-
-// var swiper = new Swiper(".home-slider", {
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-//   });
 
 
 
-let loadMoreBtn = document.querySelector('.packages .load-more .btn');
-let currentItem = 3;
 
-loadMoreBtn.onclick = () =>{
-    let boxes = [...document.querySelectorAll('.packages .box-container .box')];
-    for(var i = currentItem ; i<currentItem+3 ; i++){
-        boxes[i].style.display = 'inline-block';
-    };
-    currentItem+=3;
-    if(currentItem >= boxes.length){
-        loadMoreBtn.style.display= 'none';
-    }
+// Email send start:
+
+
+// Email validation part
+function emailValidation() {
+	var subjectEmail = document.getElementById("subjectInput").value.trim();
+	var bodyEmail = document.getElementById("bodyInput").value.trim();
+	var nameEmail = document.getElementById("nameInput").value.trim();
+	var phoneEmail = document.getElementById("phoneInput").value.trim();
+
+	if (!subjectEmail || !bodyEmail || !nameEmail) {
+		alert("Please fill out all fields before sending the email.");
+		return;
+	}
+
+	sendEmail(subjectEmail,bodyEmail,nameEmail,phoneEmail);
 }
 
 
 
 
+// send mail
+function sendEmail(subjectEmail,bodyEmail,nameEmail,phoneEmail) {
 
+	var encodedsubjectEmail = encodeURIComponent(subjectEmail);
+	var encodedbodyEmail = encodeURIComponent(bodyEmail);
+	var encodednameEmail = encodeURIComponent(nameEmail);
+	var encodedphoneEmail = encodeURIComponent(phoneEmail);
 
+	var personalEmail = "zeyadabosetta2@gmail.com";
+	
+	console.log(encodedbodyEmail);
+	console.log(encodednameEmail);
+	console.log(encodedphoneEmail);
+	console.log(encodedsubjectEmail);
 
+	var defaultMessage = "This email is from my portfolio website:\n\n";
+	var detailsMessage = "Name: "+encodednameEmail+"\nPhone: "+encodedphoneEmail+"\nSubject: "+document.getElementById("subjectInput").value+"\n\n";
 
+	var sendmsg = document.getElementById("sendmsg");
+	sendmsg.href = "mailto:" + personalEmail + "?subject=" + encodedsubjectEmail + "&body=" + defaultMessage + detailsMessage + encodedbodyEmail;
 
-Lock
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Uploading-1-image 2 MB  .........
-
-const selectImage = document.querySelector('.select-image');
-const inputFile = document.querySelector('#file');
-const imgArea = document.querySelector('.img-area');
-
-selectImage.addEventListener('click', function () {
-	inputFile.click();
-})
-
-inputFile.addEventListener('change', function () {
-	const image = this.files[0]
-	if(image.size < 2000000) {
-		const reader = new FileReader();
-		reader.onload = ()=> {
-			const allImg = imgArea.querySelectorAll('img');
-			allImg.forEach(item=> item.remove());
-			const imgUrl = reader.result;
-			const img = document.createElement('img');
-			img.src = imgUrl;
-			imgArea.appendChild(img);
-			imgArea.classList.add('active');
-			imgArea.dataset.img = image.name;
-		}
-		reader.readAsDataURL(image);
-	} else {
-		alert("Image size more than 2MB");
-	}
-})
-
-
-
-
-
-
-
-function flag_user_main(flag_main) {
-	let flag = flag_main;
-	if(flag==1){
-		document.getElementById("sign-in-btn").style.display=("none")
-	}
-
-	// Add event handler to the function
-	this.addEventListener("click", function(event) {
-		event.preventDefault();
-		return false;
-	  });
+	document.getElementById("reviewForm").reset();
 }
+
+
+// Email send end:
+
+
+
+
+
+
+
+// Handle buttons of portfolio projects Start:
+
+      var portf_boxs;
+      var portf_boxs2;
+      var frontend_box = document.querySelectorAll('.portfolio .packages .box-container .frontend');
+      var backend_box = document.querySelectorAll('.portfolio .packages .box-container .backend');
+      var portf_menu = document.querySelectorAll('.portfolio-button-menu  a');
+      
+      function portfolio_buttons(chi) {
+        portf_boxs = document.querySelectorAll('.portfolio .packages .box-container .box');
+      console.log(portf_boxs.length);
+
+        for (var i = 0; i < portf_boxs.length; i++) {
+          if(i<=4){
+          portf_menu[i].style.backgroundColor =
+            "#fff";
+          }
+             portf_boxs[i].style.display="none";
+        }
+
+        portf_boxs2=document.querySelectorAll('.portfolio .packages .box-container .'+chi);
+        for (var j = 0; j < portf_boxs2.length; j++) {
+             portf_boxs2[j].style.display="inline-block";
+        }
+        document.getElementById("portfolio-btn"+chi).style.backgroundColor = "rgb(255, 238, 55)";
+
+
+          
+      }
+
+
+
+
+
+// Handle selector of portfolio projects
+
+
+	  function portfolio_buttons_selector(chi) {
+        // console.log(chi.value);
+
+        portf_boxs = document.querySelectorAll('.portfolio .packages .box-container .box');
+        // console.log(chi.value);
+  
+        for (var i = 0; i < portf_boxs.length; i++) {
+          
+             portf_boxs[i].style.display="none";
+        }
+
+        portf_boxs2=document.querySelectorAll('.portfolio .packages .box-container .'+chi.value);
+        for (var j = 0; j < portf_boxs2.length; j++) {
+             portf_boxs2[j].style.display="inline-block";
+        }
+ 
+      }
+
+
+
+
+
+
+// Handle buttons of portfolio projects End:
 
 
 
@@ -124,22 +140,15 @@ function flag_user_main(flag_main) {
 
 	
 
-
-
-// 	// Get the element you want to change
-// var element = document.getElementById("sign-in-btn");
-
-// // Get the current value of the element
-// var currentValue = element.textContent;
-
-// // Store the current value in localStorage
-// localStorage.setItem("myElementValue", currentValue);
-
-// // Change the value of the element
-// element.textContent = "none";
-
-// // When the user returns to this page, restore the original value
-// if (localStorage.getItem("myElementValue")) {
-//   element.textContent = localStorage.getItem("myElementValue");
-// }
-// }
+window.addEventListener('scroll', function() {
+	if (window.scrollY >= 1152) {
+	  var progressbars= document.querySelectorAll('.packages .review .box-container .box .progress .progress-bar');
+	  
+	  progressbars.forEach(function(element) {
+		element.style.animation = "typing 0.7s steps(200, end)";
+	  });
+  
+	}
+	
+	
+  });
